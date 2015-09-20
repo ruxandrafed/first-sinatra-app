712d7c4f1c4d76b44dfab93c0fed0d9344657f80
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-09-20 10:20:43 -0700

    Enhancement 2 done.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ActiveRecord::Base
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :author, presence: true, length: { maximum: 25 }
-
+  validates :url, allow_blank: true, format: { :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\Z/i, :message => "Not a valid URL"}
 end
